6a2ec82945b42e1bc013a791cc8f9beb78a908d4
Santosh Narawade <santosh.narawade1@gmail.com>
2020-11-20 14:16:27 +0530

    file upload on update

--- app/controllers/course.js
+++ app/controllers/course.js
@@ -3,23 +3,43 @@
  */
 const Course = require('../models/course');
 const imageBaseName = "courseImage_";
+const fs = require('fs')
 /*!
  * Module Functions.
  */
 exports.createCourse = (req, res) => {
   let course = new Course(req.body);
   course.save();
-  uploadImage(imageBaseName + course._id, req.files.image).then(image => {
-    Course.update({ _id: course._id }, { courseImage: image }).then(done => {
-     return res.send(done);
+  if (req.files) {
+    uploadImage(imageBaseName + course._id, req.files.image).then(image => {
+      Course.update({ _id: course._id }, { courseImage: image }).then(done => {
+        return res.send(done);
+      });
     });
-  });
+  }
+  {
+    return res.send({ status: true });
+  }
   // return res.send(course);
 };
 exports.updateCourse = (req, res) => {
   let id = req.params.id;
   Course.update({ _id: id }, req.body).then(data => {
-    return res.send(data);
+    if (req.files) {
+      Course.findOne({ _id: id }).then(course => {
+        deleteFile(course.courseImage).then(done => {
+          uploadImage(imageBaseName + id, req.files ? req.files.image : null).then(image => {
+            Course.update({ _id: id }, { courseImage: image }).then(done => {
+              return res.send(done);
+            });
+          });
+        });
+      });
+    }
+    else {
+      return res.send(data);
+    }
+
   });
 };
 
@@ -38,13 +58,35 @@ exports.getCourseById = (req, res) => {
 
 exports.deleteCourse = (req, res) => {
   let id = req.params.id;
-  Course.deleteOne({ _id: id }).then(data => {
-    return res.send(data);
+  Course.findOne({ _id: id }).then(data => {
+    deleteFile(data.courseImage).then(data => {
+      Course.deleteOne({ _id: id }).then(data => {
+        return res.send(data);
+      });
+    })
   });
 };
 
+var deleteFile = fileName => {
+  return new Promise((resolve, reject) => {
+    if (fileName) {
+      fs.unlink(`./upload/${fileName}`, (err) => {
+        if (err) {
+          console.error('Error in Delete :- ', err)
+          resolve(err);
+        }
+        else {
+          resolve('done')
+        }
+      })
+    }
+    else {
+      resolve('done')
+    }
+
+  });
+}
 var uploadImage = (prefix, File) => {
-  console.log('File names before Upoload',prefix, File)
   return new Promise((resolve, reject) => {
     let name = File.name;
     let fileName = prefix + "." + name.split(".")[name.split(".").length - 1];
